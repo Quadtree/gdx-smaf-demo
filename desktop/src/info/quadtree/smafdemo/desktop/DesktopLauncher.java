c2935dd468ddb0fc6c43bc4d53ca581cc10d7f34
Quadtree <quadtree@gmail.com>
2019-04-20 10:46:05 -0700

    Basic setup

---
+++ desktop/src/info/quadtree/smafdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package info.quadtree.smafdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import info.quadtree.smafdemo.SMAFDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SMAFDemo(), config);
+	}
+}
