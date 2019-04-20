cf4d93cb90e56e366c4358e88a2b7c790f2b36db
Quadtree <quadtree@gmail.com>
2019-04-20 11:01:01 -0700

    Updated

--- core/src/info/quadtree/smafdemo/smaf/Actor.java
+++ core/src/info/quadtree/smafdemo/smaf/Actor.java
@@ -1,9 +1,28 @@
 package info.quadtree.smafdemo.smaf;
 
+import java.util.Random;
+
 public class Actor {
+    private static Random random = new Random();
+
+    private long id;
+
+    public Actor(){
+        id = random.nextLong();
+    }
+
     void enteringWorld(){}
     void exitingWorld(){}
     void update(){}
     void render(){}
     boolean keep(){ return true; }
+
+    public long getId() {
+        return id;
+    }
+
+    public Actor setId(long id) {
+        this.id = id;
+        return this;
+    }
 }
--- desktop/src/info/quadtree/smafdemo/desktop/WebSocketServer.java
+++ desktop/src/info/quadtree/smafdemo/desktop/WebSocketServer.java
@@ -1,8 +1,13 @@
 package info.quadtree.smafdemo.desktop;
 
+import javax.websocket.OnMessage;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/smafserver")
 public class WebSocketServer {
-
+    @OnMessage
+    public void testThing(String msg, Session sess){
+        System.out.println("msg=" + msg + " sess=" + sess);
+    }
 }
