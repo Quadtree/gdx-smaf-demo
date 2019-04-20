da9e3f3211f1881c8f35b10aa6624f23723167e0
Quadtree <quadtree@gmail.com>
2019-04-20 16:16:44 -0700

    Updated

--- desktop/src/info/quadtree/smafdemo/desktop/ConnectedPlayerInfo.java
+++ desktop/src/info/quadtree/smafdemo/desktop/ConnectedPlayerInfo.java
@@ -1,12 +1,21 @@
 package info.quadtree.smafdemo.desktop;
 
+import info.quadtree.smafdemo.smaf.Actor;
+
 import javax.websocket.Session;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.WeakHashMap;
 
 public class ConnectedPlayerInfo {
     private int id;
     private Session webSocketSession;
     private Instant lastMessage;
+
+    // TODO: Remove invalid replication data
+    private Map<Actor, Map<String, Object>> lastReplicatedData = new HashMap<>();
 
     public int getId() {
         return id;
@@ -33,5 +42,23 @@ public class ConnectedPlayerInfo {
     public ConnectedPlayerInfo setLastMessage(Instant lastMessage) {
         this.lastMessage = lastMessage;
         return this;
+    }
+
+    public Map<String, Object> considerReplicatingTo(Actor a){
+        Map<String, Object> newReplicationData = a.getReplicationData();
+        Map<String, Object> ret = new HashMap<>();
+
+        if (!lastReplicatedData.containsKey(a)){
+            lastReplicatedData.put(a, new HashMap<>());
+        }
+
+        for (String k : newReplicationData.keySet()){
+            if (!Objects.equals(newReplicationData.get(k), lastReplicatedData.get(a).get(k))){
+                ret.put(k, newReplicationData.get(k));
+                lastReplicatedData.get(a).put(k, newReplicationData.get(k));
+            }
+        }
+
+        return ret;
     }
 }
