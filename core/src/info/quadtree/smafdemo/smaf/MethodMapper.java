055b8b93e6d8821d76184f3b2406295f24eaef22
Quadtree <quadtree@gmail.com>
2019-04-20 21:41:55 -0700

    Changed to use strings

--- core/src/info/quadtree/smafdemo/smaf/MethodMapper.java
+++ core/src/info/quadtree/smafdemo/smaf/MethodMapper.java
@@ -3,6 +3,8 @@ package info.quadtree.smafdemo.smaf;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 import com.badlogic.gdx.utils.reflect.Method;
 import com.badlogic.gdx.utils.reflect.ReflectionException;
+import jdk.internal.org.objectweb.asm.tree.MethodNode;
+import sun.management.MethodInfo;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,37 +20,20 @@ public class MethodMapper {
         Setter
     }
 
-    private static Map<MethodInfo, Method> methodMap = new HashMap<>();
+    private static Map<String, Method> methodMap = new HashMap<>();
 
-    private static class MethodInfo {
-        private MethodInfo(String fieldName, MethodType type, Class<?> clazz) {
-            this.fieldName = fieldName;
-            this.type = type;
-            this.clazz = clazz;
-        }
-
-        String fieldName;
-        MethodType type;
-        Class<?> clazz;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            MethodInfo that = (MethodInfo) o;
-            return Objects.equals(fieldName, that.fieldName) &&
-                    type == that.type &&
-                    Objects.equals(clazz, that.clazz);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(fieldName, type, clazz);
-        }
+    private static String methodInfo(String fieldName, MethodType methodNode, Class<?> clazz){
+        StringBuilder sb = new StringBuilder();
+        sb.append(fieldName);
+        sb.append("_");
+        sb.append(methodNode);
+        sb.append("_");
+        sb.append(clazz.getName());
+        return sb.toString();
     }
 
     public static Method getGetterMethod(Class<?> clazz, String fieldName) throws ReflectionException {
-        MethodInfo mi = new MethodInfo(fieldName, MethodType.Getter, clazz);
+        String mi = methodInfo(fieldName, MethodType.Getter, clazz);
         if (!methodMap.containsKey(mi)){
             String name = generateMethodName("get", fieldName);
             methodMap.put(mi, ClassReflection.getMethod(clazz, name));
@@ -58,7 +43,7 @@ public class MethodMapper {
     }
 
     public static Method getSetterMethod(Class<?> clazz, String fieldName) throws ReflectionException {
-        MethodInfo mi = new MethodInfo(fieldName, MethodType.Setter, clazz);
+        String mi = methodInfo(fieldName, MethodType.Setter, clazz);
         if (!methodMap.containsKey(mi)){
             String name = generateMethodName("set", fieldName);
             for (Method m : ClassReflection.getMethods(clazz)){
